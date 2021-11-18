c1b42343d59be00c2b342ac98d95d9bc80068d50
Jakub Majak <j.majak.jm@gmail.com>
2021-11-18 01:47:53 +0100

    Implement Buy1Get1Free strategy

--- src/main/scala/PriceCalculationStrategy.scala
+++ src/main/scala/PriceCalculationStrategy.scala
@@ -7,11 +7,17 @@ case class StandardPriceStrategy(itemPrice: Double) extends PriceCalculationStra
 }
 
 case class Buy1Get1Free (itemPrice: Double) extends PriceCalculationStrategy {
-  def getPrice(quantity: Int): Double = -1
+  def getPrice(quantity: Int): Double = {
+    if (quantity > 1) {
+      val countedQuantity = if (quantity % 2 == 0) quantity / 2 else (quantity / 2) + 1
+      countedQuantity * itemPrice
+    }
+    else quantity * itemPrice
+  }
 }
 
 case class Buy3Pay2 (itemPrice: Double) extends PriceCalculationStrategy {
-  def getPrice(quantity: Int): Double = -1
+  def getPrice(quantity: Int): Double = 0
 }
 
 
